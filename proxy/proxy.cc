22c6fb2b758c92a96bb65f7a5359c624f53cb334
antca230 <antca230@tlhw-4-2.ad.liu.se>
2021-02-03 18:16:52 +0100

    första labben

---
+++ proxy/proxy.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10  // how many pending connections queue will hold
+
+using namespace std;
+
+int main()
+{
+
+}
