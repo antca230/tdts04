84281173e3825fe0d5c3caf830d3eff7fc751e4a
antca230 <antca230@sudent.lu.se>
2021-02-10 17:30:22 +0100

    får proxyn att skicka och ta emot websiddan

--- proxy/client.py
+++ proxy/client.py
@@ -1,57 +1,16 @@
-# import socket
-
-# # Create a socket object
-# s = socket.socket()
-
-# # Define the port on which you want to connect
-# port = 12345
-
-# # connect to the server on local computer
-# s.connect(('127.0.0.1', port))
-
-# # receive data from the server
-# print (s.recv(1024) ) 
-# # close the connection
-# s.close()
-
-# import socket
-
-# HOST = '127.0.0.1'  # The server's hostname or IP address
-# PORT = 12345        # The port used by the server
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b'Hello, world')
-#     data = s.recv(1024)
-
-# print('Received', repr(data))
-
 import sys
-
 import socket             # portable socket interface plus constants
 
 serverHost = ''          # server name, or: 'starship.python.net'
-
 serverPort = 12346                # non-reserved port used by the server
 
- 
-
 message = ['Hello network world']           # text to send to server
-
-sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # make a TCP/IP socket object
-
-sockobj.connect((serverHost, serverPort))   # connect to serve and port
-
- 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # make a TCP/IP socket object
+client.connect((serverHost, serverPort))   # connect to serve and port
 
 for line in message:
-
-    sockobj.send(line)                      # send line to server over socket
-
-    data = sockobj.recv(1024)               # receive from server: up to 1k
-
+    client.send(line)                      # send line to server over socket
+    data = client.recv(1024)               # receive from server: up to 1k
     print('Client received:', 'data')
 
- 
-
-sockobj.close()
+client.close()
--- proxy/server.py
+++ proxy/server.py
@@ -1,76 +1,35 @@
-# # first of all import the socket library
-# import socket
-
-# # next create a socket object
-# s = socket.socket()
-# print ("Socket successfully created") 
-
-# # reserve a port on your computer in our
-# # case it is 12345 but it can be anything
-# port = 12345
-
-# # Next bind to the port
-# # we have not typed any ip in the ip field
-# # instead we have inputted an empty string
-# # this makes the server listen to requests
-# # coming from other computers on the network
-# s.bind(('', port))  
-# print ("socket binded to %s" %(port))
-
-# # put the socket into listening mode
-# s.listen(5)
-# print ("socket is listening")
-
-# # a forever loop until we interrupt it or
-# # an error occurs
-# while True:
-
-# # Establish connection with client.
-#  c, addr = s.accept()
-#  print ('Got connection from', addr ) 
-
-# # send a thank you message to the client.
-#  c.send('Thank you for connecting')
-
-# # Close the connection with the client
-#  c.close()
-
-# import socket
-# import sys
-
-# HOST = "zebroid.ida.liu.se"
-# PORT = 12345
-# ipaddr = socket.gethostbyname(HOST)
-# print(socket.gethostbyname(HOST))
-
 import socket                    # get socket constructor and constants
 import os, time, sys, signal, signal
 
-myHost = ''                             # server machine, '' means local host
+myHost = ''              # server machine, '' means local host
+myPort = 12350           # listen on a non-reserved port number
+clientHost = 'zebroid.ida.liu.se'          # server name, or: 'starship.python.net'
+clientPort = 80                # non-reserved port used by the server
 
-myPort = 12346                          # listen on a non-reserved port number
-
- 
 sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # make a TCP socket object
-
 sockobj.bind((myHost, myPort))               # bind it to server port number
+sockobj.listen(1)                            # listen, allow 5 pending connects
 
-sockobj.listen(5)                            # listen, allow 5 pending connects
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # make a TCP/IP socket object
+client.connect((clientHost, clientPort)) 
 
- 
 
 while 1:                                     # listen until process killed
-
     connection, address = sockobj.accept()   # wait for next client connect
-
     print('Server connected by'), address     # connection is a new socket
-
     while 1:
+        data = connection.recv(2000)         # read next line on client socket
+       # connect to serve and port
 
-        data = connection.recv(1024)         # read next line on client socket
+        client.sendall(data)                      # send line to server over socket
+        data1 = client.recv(90000)               # receive from server: up to 1k
+       # print('Client received:', data1)
 
+        client.close()
+        connection.sendall(data1)
+        break
         if not data: break                   # send a reply line to the client
-
-        connection.send('Echo=>' + data)     # until eof when socket closed
-
+        #connection.send('Echo=>' + data)     # until eof when socket closed
     connection.close()
+    break
+
