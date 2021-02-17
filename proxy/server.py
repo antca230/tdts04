bb1fdcedb94129dcf75d494599087554c54a6700
antca230 <antca230@sudent.lu.se>
2021-02-17 13:58:24 +0100

    plz

--- proxy/server.py
+++ proxy/server.py
@@ -11,21 +11,24 @@ sockobj.bind((myHost, myPort))               # bind it to server port number
 sockobj.listen(1)                            # listen, allow 5 pending connects
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # make a TCP/IP socket object
+client.connect((clientHost, clientPort))
  
 while 1:                                     # listen until process killed
-    client.connect((clientHost, clientPort))
     (connection, address) = sockobj.accept()   # wait for next client connect
     print(f"Server connected by {address}")     # connection is a new socket
-    data = connection.recv(2000)
+    data = connection.recv(800)
     client.sendall(data) 
     while 1: 
-        data1 = client.recv(2080)               # receive from server: up to 1k
+        print("test4")
+        data1 = client.recv(800)  
+        print("test3")             # receive from server: up to 1k
+        #data1.replace("Stockholm", "Linköping")
         if(len(data1) > 0):
             connection.send(data1)
         else:
             break
     
-    client.close() 
+client.close() 
 #connection.close() 
 sockobj.close()
 print("utanför loopen")
